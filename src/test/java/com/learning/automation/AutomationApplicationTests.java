09333d2965db32faeffaa8b4d39829db94f39a9b
Satyaradha Devarapu <venkat_220@yahoo.co.in>
2022-10-21 10:10:07 -0500

    Adding application context

--- src/test/java/com/learning/automation/AutomationApplicationTests.java
+++ src/test/java/com/learning/automation/AutomationApplicationTests.java
@@ -1,13 +1,14 @@
 package com.learning.automation;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class AutomationApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Into Context loads");
 	}
 
 }

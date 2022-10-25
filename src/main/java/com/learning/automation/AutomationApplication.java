34035cf7fe3fa0c91465e04b6cc1aa9efdde06a4
manoharpulivarthi <pulivarthimanohar@gmail.com>
2022-10-25 14:47:40 -0500

    This is the new checking file.

--- src/main/java/com/learning/automation/AutomationApplication.java
+++ src/main/java/com/learning/automation/AutomationApplication.java
@@ -8,6 +8,7 @@ public class AutomationApplication {
 
 	public static void main(String[] args) {
 		System.out.println("This is the main program");
+		System.out.println("You can see this line because it is updating in Git");
 		SpringApplication.run(AutomationApplication.class, args);
 	}
 

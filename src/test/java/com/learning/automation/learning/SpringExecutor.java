6e90d78b3af3ef5a74dc6a5dd1e93540a0d7967c
Satyaradha Devarapu <venkat_220@yahoo.co.in>
2022-10-26 10:35:43 -0500

    feature/mainSource: Updated Spring profiles

--- src/test/java/com/learning/automation/learning/SpringExecutor.java
+++ src/test/java/com/learning/automation/learning/SpringExecutor.java
@@ -6,14 +6,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class SpringExecutor extends BasePage {
 
     @Value("${app.db.username}")
     public String dbUserName;
 
-    @Test
+    //@Test
     public void execute()
     {
         System.out.println("UserName Value "+dbUserName);
+    }
+
+    @Test
+    public void readProperties()
+    {
+        try
+        {
+            FileInputStream file = new FileInputStream("src//main//java//resources//credentials.properties");
+            Properties properties = new Properties();
+            properties.load(file);
+            System.out.println("Url property value: "+properties.getProperty("app1.url"));
+            System.out.println("Username property value: "+properties.getProperty("app1.db.password"));
+        }
+        catch (Exception e){}
+
     }
 }

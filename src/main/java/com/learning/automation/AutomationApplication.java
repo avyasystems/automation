74ea9208b30cfc8518a88ecbcb4c9edcc18d9cf4
Satyaradha Devarapu <venkat_220@yahoo.co.in>
2023-02-22 21:40:09 -0600

    feature/AOPAUTO-578 - Added sout statement

--- src/main/java/com/learning/automation/AutomationApplication.java
+++ src/main/java/com/learning/automation/AutomationApplication.java
@@ -9,6 +9,7 @@ public class AutomationApplication {
 	public static void main(String[] args) {
 		System.out.println("This is the main program");
 		SpringApplication.run(AutomationApplication.class, args);
+		System.out.println("Test123");
 	}
 
 }
